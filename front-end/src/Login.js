6c8110f83b90f93fe5c7e4f7f6ba4eceb19f4c1d
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2025-11-08 12:59:34 +0530

    edit

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
         if (auth) {
             navigate('/');
         }
-    });
+    },[]);
 
     const handlelogin = async () => {
         let result = await fetch("http://localhost:5400/login", {
